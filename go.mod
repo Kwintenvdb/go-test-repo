27c39cea0e6b99354549ed7ac1883fe8745f32d3
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2022-07-19 15:22:22 +0200

    Added test files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kwintenvdb/go-test-repo
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Kwintenvdb/go-test-repo/version"
+
+func main() {
+	println("Hello, world!")
+	println("Version:", version.Version)
+}
