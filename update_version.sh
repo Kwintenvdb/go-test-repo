5f4462ad98e62d36ea02d3cd67339881011fcda7
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2022-07-28 10:14:10 +0200

    Add release patch workflow

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,8 @@
+# Validate that $SPRING_VERSION conforms to the required pattern (e.g. 1.234.5)
+echo $SPRINT_VERSION | grep -Pq '^\d+\.\d+\.\d+$' && \
+    echo "Sprint version is valid." || \
+    { echo "Sprint version does not match expected pattern."; exit 1; }
+
 # Replaces the Version constant in ./version/version.go with $SPRINT_VERSION
 cat > ./version/version.go << EOM
 package version
