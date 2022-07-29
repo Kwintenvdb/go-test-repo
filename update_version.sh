4a5c8894915a633ca279f40d82e125117bb7acb2
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2022-07-29 13:50:40 +0200

    Release with .x branch suffix

--- update_version.sh
+++ update_version.sh
@@ -1,11 +1,11 @@
-# Validate that $SPRING_VERSION conforms to the required pattern (e.g. 1.234.5)
-echo $SPRINT_VERSION | grep -Pq '^\d+\.\d+\.\d+$' && \
+# Validate that $SPRINT_VERSION_WITH_PATCH conforms to the required pattern (e.g. 1.234.5)
+echo $SPRINT_VERSION_WITH_PATCH | grep -Pq '^\d+\.\d+\.\d+$' && \
     echo "Sprint version is valid." || \
     { echo "Sprint version does not match expected pattern."; exit 1; }
 
-# Replaces the Version constant in ./version/version.go with $SPRINT_VERSION
+# Replaces the Version constant in ./version/version.go with $SPRINT_VERSION_WITH_PATCH
 cat > ./version/version.go << EOM
 package version
 
-const Version = "$SPRINT_VERSION"
+const Version = "$SPRINT_VERSION_WITH_PATCH"
 EOM
